19716fd530294417d77fee835b4c3292f073d7ee
Neil Tseng <vault13tech@gmail.com>
2017-03-09 14:47:02 +0800

    加上多語言套件

---
+++ src/data/i18n_data.js
@@ -0,0 +1,20 @@
+/**
+ * 存放多語言資料的地方
+ */
+module.exports = {
+	'en': {
+		Cancel: 'Cancel',
+		Call: 'Call',
+		Sms: 'SMS',
+	},
+	'zh-TW': {
+		Cancel: '取消',
+		Call: '撥號',
+		Sms: '簡訊',
+	},
+	'zh-CN': {
+		Cancel: '取消',
+		Call: '拨号',
+		Sms: '短信',
+	},
+};
