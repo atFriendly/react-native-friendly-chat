f776e4f76842876dc1a49d8344861e710d4fcedd
Neil Tseng <vault13tech@gmail.com>
2017-03-29 16:42:57 +0800

    試著顯示全名

--- src/GiftedAvatar.js
+++ src/GiftedAvatar.js
@@ -16,14 +16,15 @@ import {
 export default class GiftedAvatar extends React.Component {
   setAvatarColor() {
     const userName = this.props.user.name || '';
-    const name = userName.toUpperCase().split(' ');
-    if (name.length === 1) {
-      this.avatarName = `${name[0].charAt(0)}`;
-    } else if (name.length > 1) {
-      this.avatarName = `${name[0].charAt(0)}${name[1].charAt(0)}`;
-    } else {
-      this.avatarName = '';
-    }
+    // const name = userName.toUpperCase().split(' ');
+    // if (name.length === 1) {
+    //   this.avatarName = `${name[0].charAt(0)}`;
+    // } else if (name.length > 1) {
+    //   this.avatarName = `${name[0].charAt(0)}${name[1].charAt(0)}`;
+    // } else {
+    //   this.avatarName = '';
+    // }
+	this.avatarName = userName;
 
     let sumChars = 0;
     for(let i = 0; i < userName.length; i++) {
